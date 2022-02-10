a5888ff0e028e8a1136009e704d9f9359f01f504
YJ_Lee <ambition65@naver.com>
2022-02-10 14:27:03 +0900

    ✨ Feat(package): baekjoon 패키지 생성
    이후 백준문제풀이는 해당 패키지 하위에 생성한다.
    백준 1181번, 1431번 풀이 완료

---
+++ src/baekjoon/sort/Q1181.java
@@ -0,0 +1,73 @@
+package baekjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+ * 
+ * 1. 길이가 짧은 것부터
+ * 2. 길이가 같으면 사전 순으로
+ */
+public class Q1181 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder builder = new StringBuilder();
+		
+		int n = Integer.parseInt(reader.readLine());
+		Word[] wordArr = new Word[n];
+		for (int i = 0; i < n; i++) {
+			String input = reader.readLine();
+			wordArr[i] = (new Word(input));
+		}
+		
+		Arrays.sort(wordArr);
+		
+		for(int i=0; i<wordArr.length; i++) {
+			String s = wordArr[i].word;
+			if(i > 0 && s.equals(wordArr[i-1].word))
+				continue;
+			
+			builder.append(s).append("\n");
+		}
+		
+		builder.deleteCharAt(builder.length()-1);
+		System.out.println(builder);
+		
+	}
+
+}
+
+class Word implements Comparable<Word> {
+	String word;
+	
+	public Word(String word) {
+		this.word = word;
+	}
+	
+	@Override
+	public int compareTo(Word o) {
+		if (this.word.length() > o.word.length()) {
+			return 1;
+			
+		} else if (this.word.length() < o.word.length()) {
+			return -1;
+			
+		} else {
+			int len = this.word.length();
+			
+			for(int i=0; i<len; i++) {
+				char a = this.word.charAt(i);
+				char b = o.word.charAt(i);
+				if((int)a == (int)b)
+					continue;
+				
+				return a - b;
+			}
+		}
+		
+		return 0;
+	}
+	
+}
---
+++ src/baekjoon/sort/Q1431.java
@@ -0,0 +1,72 @@
+package baekjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 시리얼번호 A가 시리얼번호 B의 앞에 오는 경우는 다음과 같다.
+ * 
+ * 1. A와 B의 길이가 다르면, 짧은 것이 먼저 온다.
+ * 2. 만약 서로 길이가 같다면, A의 모든 자리수의 합과 B의 모든 자리수의 합을 비교해서 작은 합을 가지는 것이 먼저온다. (숫자인 것만 더한다)
+ * 3.  만약 1,2번 둘 조건으로도 비교할 수 없으면, 사전순으로 비교한다. 숫자가 알파벳보다 사전순으로 작다.
+ */
+public class Q1431 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder builder = new StringBuilder();
+		
+		int n = Integer.parseInt(reader.readLine());
+		String[] serialArr = new String[n];
+		for(int i=0; i<serialArr.length; i++)
+			serialArr[i] = reader.readLine();
+		
+		Arrays.sort(serialArr, serialComparator);
+		
+		for(String s : serialArr)
+			builder.append(s).append("\n");
+		
+		builder.deleteCharAt(builder.length()-1);
+		System.out.println(builder);
+	}
+	
+	static Comparator<String> serialComparator = new Comparator<String>() {
+
+		@Override
+		public int compare(String o1, String o2) {
+			if(o1.length() != o2.length()) {
+				return o1.length() - o2.length();
+				
+			} else {
+				int sum1 = 0, sum2 = 0;
+				for(int i=0; i<o1.length(); i++) {
+					char c1 = o1.charAt(i);
+					char c2 = o2.charAt(i);
+					
+					if(c1 >= '1' && c1 <= '9')
+						sum1 += c1 - '0';
+					
+					if(c2 >= '1' && c2 <= '9')
+						sum2 += c2 - '0';
+				}
+				
+				if(sum1 != sum2) {
+					return sum1 - sum2;
+				} else {
+					for(int i=0; i<o1.length(); i++) {
+						char c1 = o1.charAt(i);
+						char c2 = o2.charAt(i);
+						
+						if(c1 == c2)
+							continue;
+						else
+							return c1 - c2;
+					}
+				}
+			}
+			
+			return 0;
+		}//end compare()
+	};
+}
