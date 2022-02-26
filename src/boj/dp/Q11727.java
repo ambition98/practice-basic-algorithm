e1c17115f1da7a2aeba42ede8ac0c9b91ddb3947
YJ_Lee <ambition65@naver.com>
2022-02-26 14:48:00 +0900

    ✨Feat(class): Q11727 클래스 생성

---
+++ src/boj/dp/Q11727.java
@@ -0,0 +1,25 @@
+package boj.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q11727 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] memo = new int[n + 1];
+		
+		int res = tile(n, memo);
+		
+		System.out.println(res);
+	}
+	
+	private static int tile(int n, int[] memo) {
+		if(n == 1) return 1;
+		if(n == 2) return 3;
+		if(memo[n] != 0) return memo[n];
+		
+		return memo[n] = (tile(n-2, memo)*2 + tile(n-1, memo)) % 10007;
+	}
+}
