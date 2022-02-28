92438475b43075e1878d6ae92089949c4c6c6bb0
YJ_Lee <ambition65@naver.com>
2022-02-28 16:54:00 +0900

    ✨Feat(class): Q2839 클래스 생성

---
+++ src/boj/greedy/Q2839.java
@@ -0,0 +1,37 @@
+package boj.greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q2839 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int res = n / 5;
+		int temp = n % 5;
+		
+		if(temp != 0) {
+			if(temp % 3 == 0) {
+				res += temp / 3;
+			} else {
+				while(temp < n) {
+					temp += 5;
+					res--;
+					
+					if(temp % 3 == 0) {
+						res += temp / 3;
+						temp %= 3;
+						break;
+					}
+				}
+				
+				if(temp != 0)
+					res = -1;
+			}
+		}
+		
+		System.out.println(res);
+	}
+}
